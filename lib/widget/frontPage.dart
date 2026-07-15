443f652cd75f957ce6346f0a8dbaeaeba8c3cccc
mahedizaman <info.zaman.profile@gmail.com>
2026-07-15 14:38:32 +0600

    feat: create font page header

--- lib/widget/frontPage.dart
+++ lib/widget/frontPage.dart
@@ -1,10 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class FrontPage extends StatelessWidget {
   const FrontPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Product")));
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            color: Colors.amber,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Blinkit in",
+                                style: GoogleFonts.poppins(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+
+                              Text(
+                                "16 minutes",
+                                style: GoogleFonts.poppins(
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+
+                              Row(
+                                children: [
+                                  Text(
+                                    "HOME",
+                                    style: GoogleFonts.poppins(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+
+                                  const SizedBox(width: 5),
+
+                                  Expanded(
+                                    child: Text(
+                                      "- shanirakhra, sheikhdi, jatrabari, dhaka -1236",
+                                      overflow: TextOverflow.ellipsis,
+                                      style: GoogleFonts.poppins(),
+                                    ),
+                                  ),
+
+                                  const Icon(Icons.keyboard_arrow_down),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+
+                        CircleAvatar(
+                          radius: 22,
+                          backgroundColor: Colors.white,
+                          child: Icon(Icons.person, color: Colors.black),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 15),
+
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "Search 'ice-cream'",
+                        prefixIcon: const Icon(Icons.search),
+                        suffixIcon: const Icon(Icons.mic),
+                        filled: true,
+                        fillColor: Colors.white,
+                        contentPadding: const EdgeInsets.symmetric(vertical: 0),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: Center(
+              child: Text(
+                "Body Content",
+                style: GoogleFonts.poppins(fontSize: 22),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
