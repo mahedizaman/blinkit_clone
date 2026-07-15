4ff8f17e574c3ab78f21fb0584e9eca79273a084
mahedizaman <info.zaman.profile@gmail.com>
2026-07-15 13:22:06 +0600

    feat:splash screen are working now after 3seconds

--- lib/widget/splash.dart
+++ lib/widget/splash.dart
@@ -1,8 +1,27 @@
+import 'dart:async';
+
+import 'package:blinkit_clone/widget/login.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
